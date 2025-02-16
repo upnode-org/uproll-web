8aa0b2e527df1701cd5700703c8f5a4ff5a64752
Adam Cain <03adamcain@gmail.com>
2025-02-16 15:27:37 +0000

    feat: implemented PUT and DELETE routes

--- src/app/api/configs/[id]/route.ts
+++ src/app/api/configs/[id]/route.ts
@@ -30,3 +30,62 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    if (!params?.id) {
+      return NextResponse.json({ error: "ID is required" }, { status: 400 });
+    }
+
+    const config = await prisma.configuration.findUnique({
+      where: { id: params.id },
+    });
+
+    if (!config) {
+      return NextResponse.json({ error: "Configuration not found" }, { status: 404 });
+    }
+
+    const updatedConfig = await prisma.configuration.update({
+      where: { id: params.id },
+      data: {
+        ...(await req.json())
+      }
+    });
+
+    return NextResponse.json(updatedConfig);
+  } catch (error) {
+    console.error("Error updating configuration:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    if (!params?.id) {
+      return NextResponse.json({ error: "ID is required" }, { status: 400 });
+    }
+
+    const config = await prisma.configuration.findUnique({
+      where: { id: params.id },
+    });
+
+    if (!config) {
+      return NextResponse.json({ error: "Configuration not found" }, { status: 404 });
+    }
+
+    await prisma.configuration.delete({
+      where: { id: params.id },
+    });
+
+    return NextResponse.json({ message: "Configuration deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting configuration:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
