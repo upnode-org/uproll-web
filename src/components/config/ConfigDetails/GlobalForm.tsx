9d8e091ecdd1cb58f7ce18fc9fe5f64f3c0d6ead
Adam Cain <03adamcain@gmail.com>
2025-02-25 09:32:44 +0000

    feat: add global tolerations field to configuration form

--- src/components/config/ConfigDetails/GlobalForm.tsx
+++ src/components/config/ConfigDetails/GlobalForm.tsx
@@ -2,6 +2,7 @@ import { LOG_LEVELS, LogLevel } from "@/lib/configSchema";
 import FormSelect from "./Components/FormSelect";
 import FormCheckbox from "./Components/FormCheckbox";
 import FormNodeSelectorFields from "./Components/FormRecordFields";
+import FormTolerationsField from "./Components/FormTolerationFieldArray";
 export default function GlobalForm() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
@@ -10,12 +11,16 @@ export default function GlobalForm() {
         options={LOG_LEVELS}
         label="Global Log Level"
       />
-        <FormCheckbox
-          watchName="optimism_package.persistent"
-          label="Persistent" />
+      <FormCheckbox
+        watchName="optimism_package.persistent"
+        label="Persistent" />
       <FormNodeSelectorFields
         label="Global Node Selectors"
         fieldArrayName="optimism_package.global_node_selectors"
+      />
+      <FormTolerationsField
+        label="Global Tolerations"
+        fieldArrayName="optimism_package.global_tolerations"
       />
     </div>
   )
