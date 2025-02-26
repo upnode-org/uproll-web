e5f8dd5dbf80ea6fa1425b3b2c5e3bdae08ad910
Adam Cain <03adamcain@gmail.com>
2025-02-26 22:20:15 +0000

    refactor: remove description field from Configuration model and migrate db

---
+++ prisma/migrations/20250226221826_remove_field_from_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Configuration` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Configuration" DROP COLUMN "description";
