01ed2daef21e785451795a6097a9c4a9b709c4bb
Adam Cain <03adamcain@gmail.com>
2025-02-27 13:09:57 +0000

    fix: refine homepage title and padding responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,11 @@ export default function HomePage() {
           <div className="grid grid-cols-1 md:grid-cols-2 max-w-screen-xl mx-auto relative">
             {/* Left Grid Cell */}
             <div className="relative uppercase w-full flex items-center">
-              <div className="mx-auto p-2 sm:p-8">
+              <div className="mx-auto px-1 py-6 sm:p-5 md:p-8">
                 <p className="text-white text-sm">
                   The easiest way to spin up your OP stack rollups
                 </p>
-                <h1 className="text-white text-8xl font-bold">Uproll CLI</h1>
+                <h1 className="text-white text-8xl font-bold flex-wrap">Uproll CLI</h1>
                 <Link href="/config">
                   <Button className="mt-4">Get Started</Button>
                 </Link>
