e4afc4602ab54eb1c1202ff2920dbc9561cce7ec
Adam Cain <03adamcain@gmail.com>
2025-02-28 09:57:29 +0000

    fix: adjust homepage hero section vertical padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
           <div className="grid grid-cols-1 md:grid-cols-2 max-w-screen-xl mx-auto relative">
             {/* Left Grid Cell */}
             <div className="relative uppercase w-full flex items-center">
-              <div className="mx-auto px-5 py-8 md:px-5 lg:p-10">
+              <div className="mx-auto px-5 py-14 md:px-5 lg:px-10">
                 <p className="text-white text-sm">
                   The easiest way to spin up your OP stack rollups
                 </p>
