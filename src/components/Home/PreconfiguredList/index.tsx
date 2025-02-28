54b9609ea83b0cc0fdef4d341d752907139505d6
Adam Cain <03adamcain@gmail.com>
2025-02-28 12:47:55 +0000

    refactor: rename Index component to PreconfiguredList for clarity

--- src/components/Home/PreconfiguredList/index.tsx
+++ src/components/Home/PreconfiguredList/index.tsx
@@ -3,7 +3,7 @@ import VerticalAnimatedLinks from "./VerticalAnimatedLinks";
 import HorizontalAnimatedLinks from "./HorizontalAnimatedLinks";
 import { useIsMobile } from '@/hooks/use-mobile';
 
-export default function Index() {
+export default function PreconfiguredList() {
     const isMobile = useIsMobile(1024);
 
     return (
