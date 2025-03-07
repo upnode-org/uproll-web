2a5b7a7c44cfd04f6ddc2694d3c75de7033f9c62
Adam Cain <03adamcain@gmail.com>
2025-03-07 11:13:41 +0000

    feat: make select field dynamic

--- src/components/config/RollupConfig/Components/SelectField.tsx
+++ src/components/config/RollupConfig/Components/SelectField.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Controller, Control } from "react-hook-form";
+import { Controller, Control, get, useFormContext, FieldPathByValue, useWatch } from "react-hook-form";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -11,25 +11,29 @@ import {
 } from "@/components/ui/select";
 import { ErrorMessage } from "./ErrorMessage";
 import { RollupConfig } from "@/lib/opSchema";
+import { EnumLike } from "zod";
 
 export type Option = { label: string; value: string };
 
 export type SelectFieldProps = {
   label: string;
   options: Option[];
-  control: Control<RollupConfig>;
-  name: string;
-  error?: string;
+  name: FieldPathByValue<RollupConfig, EnumLike | undefined>;
 };
 
 export const SelectField: React.FC<SelectFieldProps> = ({
   label,
   options,
-  control,
   name,
-  error,
 }) => {
+  if(name === undefined) {
+    throw new Error("Name is undefined");
+  }
+
+  const { control, formState: { errors } } = useFormContext<RollupConfig>();
+
   const errorId = `${name}-error`;
+  const errorMessage = get(errors, name);
   return (
     <div className="space-y-1">
       <Label className="text-sm font-medium text-gray-700">{label}</Label>
@@ -41,10 +45,10 @@ export const SelectField: React.FC<SelectFieldProps> = ({
             onValueChange={field.onChange}
             value={field.value as string}
             defaultValue={field.value as string}
-            aria-describedby={error ? errorId : undefined}
-            aria-invalid={!!error}
+            aria-describedby={errorMessage ? errorId : undefined}
+            aria-invalid={!!errorMessage}
           >
-            <SelectTrigger className={`bg-white ${error ? "border-red-500" : ""}`}>
+            <SelectTrigger className={`bg-white ${errorMessage ? "border-red-500" : ""}`}>
               <SelectValue placeholder="Select an option" />
             </SelectTrigger>
             <SelectContent>
@@ -57,7 +61,7 @@ export const SelectField: React.FC<SelectFieldProps> = ({
           </Select>
         )}
       />
-      <ErrorMessage id={errorId} error={error} />
+      {errorMessage && <ErrorMessage id={errorId} error={errorMessage} />}
     </div>
   );
 };
