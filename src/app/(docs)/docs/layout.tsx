56cd7e8e16fad2724a8d5bdb718e93654c087daa
Adam Cain <03adamcain@gmail.com>
2025-05-14 09:26:34 +0100

    refactor: update documentation layout to restrict pageMap to only docs mdx content

--- src/app/(docs)/docs/layout.tsx
+++ src/app/(docs)/docs/layout.tsx
@@ -3,26 +3,42 @@ import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import GradientBackground from '@/components/GradientBackground'
 
+interface PageItem {
+    name: string;
+    route: string;
+    title: string;
+    children?: PageItem[];
+    frontMatter?: {
+        title: string;
+        filePath: string;
+        timestamp: number;
+    };
+}
+
 export default async function DocLayout({ children }: { children: React.ReactNode }) {
+
+    const pageMap = await getPageMap() as PageItem[]
+    const docs = pageMap.find(page => page.name === 'docs')?.children || []
+
     return (
         <div className='max-h-[100dvh] overflow-hidden'>
             <Layout
                 nextThemes={{
                     defaultTheme: "light",
-                // forcedTheme: "light",
-            }}
-            pageMap={await getPageMap()}
-            // pageMap={[{}] as PageMapItem[]}
-            docsRepositoryBase="https://github.com/upnode-org/uproll-web/tree/main/content"
-        >
-            <div className='h-26 w-full'>
-                <GradientBackground className='h-full w-full' />
-            </div>
-            <div className='container mx-auto'>
-                <Navbar align='left' logo={<span />} />
-                {children}
-            </div>
-        </Layout>
+                    // forcedTheme: "light",
+                }}
+                pageMap={docs}
+                // pageMap={[{}] as PageMapItem[]}
+                docsRepositoryBase="https://github.com/upnode-org/uproll-web/tree/main/content"
+            >
+                <div className='h-26 w-full'>
+                    <GradientBackground className='h-full w-full' />
+                </div>
+                <div className='container mx-auto'>
+                    <Navbar align='left' logo={<span />} />
+                    {children}
+                </div>
+            </Layout>
         </div>
     )
 }
